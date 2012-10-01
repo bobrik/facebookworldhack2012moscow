7cd35b986dc16936c5466bbb154683fca3d79246
Simon <s.shiharev@topface.com>
2012-10-01 17:52:51 +0400

    popup

--- public/js/hang.js
+++ public/js/hang.js
@@ -35,7 +35,7 @@ var H = {
             var documentWidth = window.innerWidth / 2 + window.pageXOffset - (block.offsetWidth / 2);
             var documentHeight = window.innerHeight / 2 + window.pageYOffset - (block.offsetHeight / 2);
 
-            overlay.style.height = window.document.offsetHeight + "px";
+            overlay.style.height = document.bodyoffsetHeight + "px";
 
             block.style.left = documentWidth + "px";
             block.style.top = documentHeight + 50 + "px";
