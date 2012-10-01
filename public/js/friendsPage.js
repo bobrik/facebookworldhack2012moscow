34dbfb33eab442de036a23da407dc6f481ccce8c
Simon <s.shiharev@topface.com>
2012-10-01 16:47:55 +0400

    popup

--- public/js/friendsPage.js
+++ public/js/friendsPage.js
@@ -2,7 +2,8 @@ H.pages.friendsPage = function() {
     var list = document.querySelector("ul.itemList");
     console.log(list)
     list.addEventListener("click", function(event){
-        var elm = H.closest("li", event.target || event.srcElement);
+        var elm = H.closest("li", event.srcElement);
+        console.log(elm)
         if (elm && elm.dataset.installed) {
             H.ajaxPost("/create", {
                 id: elm.dataset.fbid,
