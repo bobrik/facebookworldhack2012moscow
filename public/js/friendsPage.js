03e4fa9294789d4458e399401c9c48c667b05188
Simon <s.shiharev@topface.com>
2012-10-01 16:13:25 +0400

    popup

--- public/js/friendsPage.js
+++ public/js/friendsPage.js
@@ -1,3 +1,6 @@
 H.pages.friendsPage = function() {
-
+    var list = document.querySelector("ul.itemList");
+    list.addEventListener("click", function(event){
+        console.log(JSON.stringify(event))
+    });
 }
