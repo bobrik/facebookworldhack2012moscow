0516cae2859c20d7d4fc0a3a0c97c1f66287ab55
Simon <s.shiharev@topface.com>
2012-10-01 13:30:04 +0400

    button and more

--- public/js/startPage.js
+++ public/js/startPage.js
@@ -1,4 +1,4 @@
-H.page.startPage = function() {
+H.pages.startPage = function() {
     var startButton = H.ge("startGame");
     startButton.addEventListener("click", function(){
         H.popup.show({title: "Start Game"});
