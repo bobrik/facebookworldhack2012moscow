7570b1706c396da01115586590d4eeed8a8351a6
Simon <s.shiharev@topface.com>
2012-10-01 14:45:17 +0400

    popup

--- public/js/startPage.js
+++ public/js/startPage.js
@@ -1,6 +1,6 @@
 H.pages.startPage = function() {
     var startButton = H.ge("startGame");
     startButton.addEventListener("click", function(){
-        H.popup.show({title: "Start Game", content: "<a id='123'>Buttton!!!</a>"});
+        H.popup.show({title: "Start Game", content: "<a class='button' id='friends'>With Friends</a><a class='button'  id='random'>With Random User</a>"});
     });
 }
