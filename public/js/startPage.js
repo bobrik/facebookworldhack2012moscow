2dee6cfa5310d1424daafa55910138e295e41e0f
Simon <s.shiharev@topface.com>
2012-10-01 18:39:37 +0400

    gammeee!

--- public/js/startPage.js
+++ public/js/startPage.js
@@ -1,9 +1,9 @@
 H.pages.startPage = function() {
-    var startButton = H.ge("startGame");
-    startButton.addEventListener("click", function(){
-        H.popup.show({title: "Start Game", content: "<a class='button' href='/friends' id='friends'>With Friends</a>" +
-            "<a class='button'  href='/random'   id='random'>With Random User</a>"});
-    });
+//    var startButton = H.ge("startGame");
+//    startButton.addEventListener("click", function(){
+//        H.popup.show({title: "Start Game", content: "<a class='button' href='/friends' id='friends'>With Friends</a>" +
+//            "<a class='button'  href='/random'   id='random'>With Random User</a>"});
+//    });
 
     var list = document.querySelector("ul.itemList");
     list.addEventListener("click", function(event){
