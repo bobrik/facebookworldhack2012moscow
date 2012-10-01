c2db4ace762ddf5e89e14ed67d8b4c677e67f087
Simon <s.shiharev@topface.com>
2012-10-01 18:54:03 +0400

    gammeee!

--- public/js/fieldPage.js
+++ public/js/fieldPage.js
@@ -1,10 +1,9 @@
 H.pages.fieldPage = function() {
-    alert(1)
     var list = document.querySelector(".keyboard .letter"),
         gameId = document.querySelector("#field").dataset.gameid;
     list.addEventListener("click", function(event) {
         H.ajaxPost("/move", {
-            gameId: event.target.html(),
+            gameId: event.target.innerHTML,
             letter: gameId
         });
     });
