eb3b2dbcfa36893f1b81515d03543f20f0b0b95d
Andrey Mischenko <git@gildor.ru>
2012-10-01 18:34:11 +0400

    keyboard

--- public/js/fieldPage.js
+++ public/js/fieldPage.js
@@ -1,3 +1,10 @@
 H.pages.fieldPage = function() {
-
+    var list = document.querySelector(".keyboard.letter"),
+        gameId = document.querySelector("#field").dataset.gameid;
+    list.addEventListener("click", function(event) {
+        H.ajaxPost("/move", {
+            gameId: event.target.html(),
+            letter: gameId
+        });
+    });
 };
--- routes/field.js
+++ routes/field.js
@@ -18,7 +18,8 @@
                 res.render('field', {
                     pageName : "field",
                     title    : 'Game',
-                    game: game
+                    game: game,
+                    keyboard: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
                 });
             });
         });
