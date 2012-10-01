7f48ec04549426960ec295ccf825652ffae382b0
bobrik <ibobrik@gmail.com>
2012-10-01 15:51:57 +0400

    word fixes

--- routes/og_word.js
+++ routes/og_word.js
@@ -9,9 +9,9 @@
         }
 
         res.render("og_word", {
-            title : 'Hangman: ' + req.query.word,
+            title : req.query.word,
+            length: req.quert.word.length,
             pageName: "og_word",
-            name  : req.query.word,
             image : "http://cs302709.userapi.com/v302709982/81a9/V4STjJ8buiI.jpg",
             url   : "http://web372.verumnets.ru/og_word?word=" + req.query.word
         });
