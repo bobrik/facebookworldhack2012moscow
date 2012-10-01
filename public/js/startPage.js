6d3cd6ce364829fcb2d83e3cecc5081b77386a73
Simon <s.shiharev@topface.com>
2012-10-01 13:32:03 +0400

    start button poup

--- public/js/startPage.js
+++ public/js/startPage.js
@@ -1,4 +1,5 @@
 H.pages.startPage = function() {
+    alert(1)
     var startButton = H.ge("startGame");
     startButton.addEventListener("click", function(){
         H.popup.show({title: "Start Game"});
