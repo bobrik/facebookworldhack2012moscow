ab47741043721f66200365a71c627f36e58f1783
Simon <s.shiharev@topface.com>
2012-10-01 18:52:44 +0400

    gammeee!

--- public/js/fieldPage.js
+++ public/js/fieldPage.js
@@ -1,4 +1,5 @@
 H.pages.fieldPage = function() {
+    alert(1)
     var list = document.querySelector(".keyboard .letter"),
         gameId = document.querySelector("#field").dataset.gameid;
     list.addEventListener("click", function(event) {
