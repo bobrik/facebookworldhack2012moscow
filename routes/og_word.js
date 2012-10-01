464aca8e24519d4ed77f4407f73b7f2f786f4c3c
bobrik <ibobrik@gmail.com>
2012-10-01 15:52:32 +0400

    word fixes

--- routes/og_word.js
+++ routes/og_word.js
@@ -10,7 +10,7 @@
 
         res.render("og_word", {
             title : req.query.word,
-            length: req.quert.word.length,
+            length: req.query.word.length,
             pageName: "og_word",
             image : "http://cs302709.userapi.com/v302709982/81a9/V4STjJ8buiI.jpg",
             url   : "http://web372.verumnets.ru/og_word?word=" + req.query.word
