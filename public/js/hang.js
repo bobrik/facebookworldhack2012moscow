c6d743a81ecad9090de3a47c6b100aad63e7578c
Simon <s.shiharev@topface.com>
2012-10-01 16:51:45 +0400

    popup

--- public/js/hang.js
+++ public/js/hang.js
@@ -56,6 +56,7 @@ var H = {
 
 
     closest: function(nodeName, elm) {
+        elm && console.log (elm.nodeName, elm.parentNode)
         if (elm && elm.nodeName.toLowerCase() != nodeName.toLowerCase()) {
             if (elm && elm.parentNode) {
                  return H.closest(nodeName, elm.parentNode);
