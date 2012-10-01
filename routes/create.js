315b68e06e4feffdbb819d51ffd2f824e25d3b37
bobrik <ibobrik@gmail.com>
2012-10-01 17:09:08 +0400

    random user

--- routes/create.js
+++ routes/create.js
@@ -1,25 +1,40 @@
 (function(module) {
-    var authorized = require("./authorized");
+    var authorized = require("./authorized"),
+        User       = require("../lib/User");
 
     module.exports = authorized(function(req, res, next, user) {
-        if (!req.body.id || !req.body.word) {
+        if (!req.body.word) {
             return next(new Error("No required params!"));
         }
 
-        user.createGame(req.body.id, req.body.word, function(error, game) {
-            if (error) {
-                return next(error);
-            }
-
-            game.exportFor(user, function(error, data) {
+        function creator(id) {
+            user.createGame(opponent.getId(), req.body.word, function(error, game) {
                 if (error) {
                     return next(error);
                 }
 
-                res.json({
-                    game: data
+                game.exportFor(user, function(error, data) {
+                    if (error) {
+                        return next(error);
+                    }
+
+                    res.json({
+                        game: data
+                    });
                 });
             });
-        });
+        }
+
+        if (!req.body.id) {
+            User.getRandom(function(error, opponent) {
+                if (error) {
+                    return next(error);
+                }
+
+                creator(opponent.getId());
+            });
+        } else {
+            creator(req.body.id);
+        }
     });
 })(module);
