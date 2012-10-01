f74e5c3e41ceff497b3e6323d7044ac1c0c089ff
Simon <s.shiharev@topface.com>
2012-10-01 17:19:50 +0400

    popup

--- public/js/friendsPage.js
+++ public/js/friendsPage.js
@@ -1,14 +1,50 @@
 H.pages.friendsPage = function() {
     var list = document.querySelector("ul.itemList");
-    console.log(list)
     list.addEventListener("click", function(event){
+
+        var cnt = "";
+        var words = H.getWords();
         var elm = H.closest("li", event.target);
-        console.log(elm, event.target)
-        if (elm && elm.dataset.installed) {
-            H.ajaxPost("/create", {
-                id: elm.dataset.fbid,
-                word: "test"
-            })
+
+        for (var i in words) {
+            cnt +=  "<a class='button word' href='javascript:;'>" + words[i] + "</a>";
         }
+
+        H.popup.show({
+            title: "Choose the word",
+            content: cnt
+        });
+
+        var elms = document.querySelectorAll(".word");
+
+        for (var i in elms) {
+            elms[i].addEventListener("click", function(event){
+                H.popup.hide();
+                if (elm && elm.dataset.installed) {
+                    H.ajaxPost("/create", {
+                        id: elm.dataset.fbid,
+                        word: event.target.innerText
+                    });
+
+                } else {
+                    H.initFbAPI(function(){
+                        FB.ui({method: 'apprequests',
+                            message: 'Hey! Play with me!',
+                            to: elm.dataset.fbid
+                        }, function(resp){
+                            if (resp && resp.to) {
+                                H.ajaxPost("/create", {
+                                    id: elm.dataset.fbid,
+                                    word: event.target.innerText
+                                });
+                            }
+                        });
+                    });
+                }
+            });
+        }
+
+
+
     });
 }
